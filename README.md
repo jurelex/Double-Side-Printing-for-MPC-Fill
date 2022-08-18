fb72a183576de1be4f44d4751992d1c68dbcee8a
Renato Patron <jurelex@outlook.com>
2022-08-17 22:45:06 -0700

    Added new feature for merging multiple decks

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 <p>Pyhton script to merge Magic the Gathering decks.</p>
 It creates a CSV file to be used at MPCfill.com<br>
 It has been tested with txt files downloaded from mtggoldfish.com<br>
-The file merge.py and the 2 .txt deck files (front.txt and back.txt) need to be in the same folder.<br>
+<s>The file merge.py and the 2 .txt deck files (front.txt and back.txt) need to be in the same folder.</s><br>
 To run the script type:<br>
 <i>python merge.py</i><br>
 The file merged.csv will be generated. Upload that file to mpcfill.com<br>
+ changelog:<br>
+    0.2 = Added a new feature: merging multiple decks for front and back cards. Decklist can be added directly to <br>
+    the front_decks and back_decks folders. Also created a temp folder to keep temporary files in one place.
+    
 
 Created with python 3.10.
--- merge.py
+++ merge.py
@@ -3,29 +3,53 @@
 """
     __author__ = "Renato Patron"
     __license__ = "GPL"
-    __version__ = "0.1"
+    __version__ = "0.2"
     __maintainer__ = "Jurelex"
     __email__ = "jurelex@outlook.com"
-    __status__ = "Production`
+    __status__ = "Production"
+    changelog:
+    0.2 = Added a new feature: merging multiple decks for front and back cards. Decklist can be added directly to 
+    the front_decks and back_decks folders. Also created a temp folder to keep temporary files in one place.
+    
 """
 # Imports
 
+import glob
 import itertools
+import shutil
+
+# Merges deckfiles in front_decks directory
+with open("temp/front.txt", 'wb') as outfile:
+    for filename in glob.glob('front_decks/*.txt'):
+        if filename == "front.txt":
+            # don't want to copy the output into the output
+            continue
+        with open(filename, 'rb') as readfile:
+            shutil.copyfileobj(readfile, outfile)
+            
+# Merges deckfiles in back_decks directory
+with open("temp/back.txt", 'wb') as outfile:
+    for filename in glob.glob('back_decks/*.txt'):
+        if filename == "back.txt":
+            # don't want to copy the output into the output
+            continue
+        with open(filename, 'rb') as readfile:
+            shutil.copyfileobj(readfile, outfile)
 
 # Creates out1.txt and out2.txt files after deleting any blank lines in the front.txt and back.txt files.
-open('out1.txt','w').write(
+open('temp/out1.txt','w').write(
     ''.join(
         l for l in open(
-            'front.txt') if l.strip())) 
-open('out2.txt','w').write(
+            'temp/front.txt') if l.strip())) 
+open('temp/out2.txt','w').write(
     ''.join(
         l for l in open(
-            'back.txt') if l.strip())) 
+            'temp/back.txt') if l.strip())) 
 
 # Creates deck1.txt and deck2.txt.
 
-f_1 = open("out1.txt", "r")
-with open("deck1.txt", "w") as w:
+f_1 = open("temp/out1.txt", "r")
+with open("temp/deck1.txt", "w") as w:
     for x in f_1:
         # Stores the amount of times a card is included in the deck
         times = int(x[0:2])
@@ -38,8 +62,8 @@ with open("deck1.txt", "w") as w:
             w.write(f'"{clean_card.strip()}"\n')
             times -=1
             
-f_2 = open("out2.txt", "r")
-with open("deck2.txt", "w") as w:
+f_2 = open("temp/out2.txt", "r")
+with open("temp/deck2.txt", "w") as w:
     for x in f_2:
         # Stores the amount of times a card is included in the deck
         times = int(x[0:2])
@@ -54,9 +78,9 @@ with open("deck2.txt", "w") as w:
 
 # Uses the deck1.txt and deck2.txt files and merges them as a CSV file following the template in 
 # https://mpcfill.com/static/cardpicker/template.9e9a90cf46aa.csv
-filenames = ['deck1.txt', 'deck2.txt']
-
-with open('deck1.txt') as src1, open('deck2.txt', 'r') as src2, open('merged.csv', 'w') as dst:
+filenames = ['temp/deck1.txt', 'temp/deck2.txt']
+# Outputs CSV file in CSV folder
+with open('temp/deck1.txt') as src1, open('temp/deck2.txt', 'r') as src2, open('CSV/merged.csv', 'w') as dst:
     dst.write(f"Quantity, Front, Back\n")
     for line_from_first, line_from_second in itertools.zip_longest(src1, src2):
         if line_from_first is not None:
